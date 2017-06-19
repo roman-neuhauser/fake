b6c144cd94d15a477b4d8c5311d54ffc593473e4
Roman Neuhauser <roman@sigpipe.cz>
2017-06-19 11:18:08 +0200

    p/brew: Homebrew formula sketch

---
+++ p/brew/fake.rb
@@ -0,0 +1,16 @@
+class Fake < Formula
+  desc "Mocks and Stubs For the Command Line"
+  homepage "http:s//github.com/roman-neuhauser/fake"
+  version "__VERSION__"
+  url "https://github.com/roman-neuhauser/fake/archive/v#{version}.tar.gz"
+  sha256 ""
+  head "https://github.com/roman-neuhauser/fake.git"
+
+  depends_on "basexy"
+  depends_on "cram" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "check", "install"
+  end
+end
